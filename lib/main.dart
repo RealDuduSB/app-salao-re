459035bf40065ce261824f1660774244f417f14e
Eduardo Silva Borges <edusborges99@hotmail.com>
2022-03-28 20:26:54 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:sbs_app/pages/login/login.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Login(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
